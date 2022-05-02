6652139b50333f8a01e345df5cd9cda0037f09c8
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-05-02 13:47:58 -0500

    commit tree 9dc678506c64302c80f5bf64eb9983ee34466063

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,19 @@
 class Author < ApplicationRecord
+    validates :name, presence: true
+    validates :name, uniqueness: {case_sensitive: true}
+    validates :phone_number, length: { is: 10 }
+
+    def create
+        author = Author.create!(author_params)
+        render json: author, status: :created
+    rescue ActiveRecord::RecordInvalid => invalid 
+        render json:{error: invalid.record.errors}, status: :unprocessable_entity
+    end
+
+    private
+
+    def author_params
+        params.permit(:name, :phone_number)
+    end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,30 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_is_clickbait
+
+    def create
+        post = Post.create!(post_params)
+        render json: post, status: :created
+    rescue ActiveRecord::RecordInvalid => invalid 
+        render json:{error: invalid.record.errors}, status: :unprocessable_entity
+    end
+
+    private
+
+    def post_params
+        params.permit(:title, :content, :summary, :category)
+    end
+
+    def title_is_clickbait 
+        array = ["Won't Believe", "Secret", "Top", "Guess"]
+        if array do |phrase| 
+            title.match? phrase
+            errors.add(:title, "error")
+        end
+    end
+
 end
